be5fb48026f4d467183e8ae63edf63a5fc6492cd
Martin McEvoy <martin@weborganics.co.uk>
2010-05-16 16:26:17 +0100

    Version 1.2.1 new template

--- app/template/head.php
+++ app/template/head.php
@@ -2,10 +2,10 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
-<title>TransFormr - Microformats Transformer Toolkit</title>
+<title>Microform.at Transformr</title>
 <meta name="microid" content="mailto+http:sha1:d80c1d056342d40536976ff87df5212b076ff8d8">
 <meta name="keywords" content="hCard Transformer,hAtom Transformer,hCalendar Transformer,Geo Transformer">
-<meta name="description" content="Microformats Transformer Toolkit">
+<meta name="description" content="A Microformats Transformer">
 <link type="application/atom+xml" title="TransFormr Updates Feed" href="http://github.com/feeds/WebOrganics/commits/TransFormr/master" rel="alternate">
 <link type="application/rdf+xml" title="DOAP" href="<?php echo $this->path; ?>doap.rdf" rel="meta">
 <link rel="icon" href="<?php echo $this->path;?>favicon.ico" type="image/x-icon" />
